6d136376b2827f3424aee57a1075d53578c22b85
Pandula <43129749+p4nduxcv@users.noreply.github.com>
2023-01-27 01:15:34 +0530

    assignment 1 related to the list and conditional rendering

--- code/02-understanding-keys/src/components/Expenses/Expenses.js
+++ code/02-understanding-keys/src/components/Expenses/Expenses.js
@@ -12,6 +12,10 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  const filterExpenses = props.items.filter( exp => {
+    return exp.date.getFullYear().toString() === filteredYear
+  })
+
   return (
     <div>
       <Card className='expenses'>
@@ -19,7 +23,8 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {props.items.map((expense) => (
+
+        {filterExpenses.map((expense) => (
           <ExpenseItem
             key={expense.id}
             title={expense.title}
